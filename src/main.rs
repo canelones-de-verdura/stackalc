20f0b9d79b5e2b305e37238eed1c641ef20b4a95
Eze <eze.gonzalez@vera.com.uy>
2026-04-15 20:13:02 -0300

    refactor importante

--- src/main.rs
+++ src/main.rs
@@ -1,91 +1,214 @@
-use std::io;
+use std::{collections::HashMap, io};
 
 enum Operator {
+    // stack
     Load(f64),
     Neg,
     Add,
     Sub,
     Mul,
     Div,
+    Dup,
+    Pop,
+
+    // lógicos
+    Ceq,
+    Cgt,
+    Clt,
+
+    // vars
+    Ldv(usize),
+    Stv(usize),
 }
 
 #[derive(Debug)]
 enum StackalcError {
     EmptyStack,
+    EmptyVariable,
+    DivByZero,
+    BadOperator,
 }
 
-fn execute_cmd(cmd: Operator, stack: &mut Vec<f64>) -> Result<(), StackalcError> {
-    let mut double_pop = || {
-        let arg2 = stack.pop().ok_or(StackalcError::EmptyStack)?;
-        let arg1 = stack.pop().ok_or(StackalcError::EmptyStack)?;
+struct Interpreter {
+    stack: Vec<f64>,
+    vars: HashMap<usize, f64>,
+}
 
+impl Interpreter {
+    fn new() -> Self {
+        Self {
+            stack: Vec::<f64>::with_capacity(100),
+            vars: HashMap::<usize, f64>::new(),
+        }
+    }
+
+    fn peek(&mut self) -> Result<f64, StackalcError> {
+        let len = self.stack.len();
+        if len == 0 {
+            return Err(StackalcError::EmptyStack);
+        }
+
+        let arg = self.stack[len - 1];
+        Ok(arg)
+    }
+
+    fn pop(&mut self) -> Result<f64, StackalcError> {
+        let last = self.stack.pop().ok_or(StackalcError::EmptyStack)?;
+        Ok(last)
+    }
+
+    fn double_pop(&mut self) -> Result<(f64, f64), StackalcError> {
+        let arg2 = self.stack.pop().ok_or(StackalcError::EmptyStack)?;
+        let arg1 = self.stack.pop().ok_or(StackalcError::EmptyStack)?;
         Ok((arg1, arg2))
-    };
+    }
 
+    fn push(&mut self, num: f64) {
+        self.stack.push(num);
+    }
+
+    fn store(&mut self, key: usize) -> Result<(), StackalcError> {
+        let val = self.pop()?;
+        self.vars.insert(key, val);
+
+        Ok(())
+    }
+
+    fn load(&mut self, key: usize) -> Result<(), StackalcError> {
+        let num = self.vars.get(&key).ok_or(StackalcError::EmptyVariable)?;
+        self.push(num.clone());
+
+        Ok(())
+    }
+}
+
+fn execute_cmd(cmd: Operator, vm: &mut Interpreter) -> Result<(), StackalcError> {
     match cmd {
         Operator::Load(num) => {
-            stack.push(num);
+            vm.push(num);
         }
         Operator::Neg => {
-            let arg = stack.pop().ok_or(StackalcError::EmptyStack)?;
-            stack.push(-arg);
+            let arg = vm.pop()?;
+            vm.push(-arg);
         }
         Operator::Add => {
-            let (arg1, arg2) = double_pop()?;
-            stack.push(arg1 + arg2);
+            let (arg1, arg2) = vm.double_pop()?;
+            vm.push(arg1 + arg2);
         }
         Operator::Sub => {
-            let (arg1, arg2) = double_pop()?;
-            stack.push(arg1 - arg2);
+            let (arg1, arg2) = vm.double_pop()?;
+            vm.push(arg1 - arg2);
         }
         Operator::Mul => {
-            let (arg1, arg2) = double_pop()?;
-            stack.push(arg1 * arg2);
+            let (arg1, arg2) = vm.double_pop()?;
+            vm.push(arg1 * arg2);
         }
         Operator::Div => {
-            let (arg1, arg2) = double_pop()?;
-            stack.push(arg1 / arg2); // TODO da NaN. q hacemos?
+            let (arg1, arg2) = vm.double_pop()?;
+            if arg2 == 0f64 {
+                return Err(StackalcError::DivByZero);
+            }
+            vm.push(arg1 / arg2);
         }
+        Operator::Dup => {
+            let arg = vm.peek()?;
+            vm.push(arg);
+        }
+        Operator::Pop => {
+            vm.pop()?;
+        }
+
+        Operator::Ceq => {
+            let (arg1, arg2) = vm.double_pop()?;
+            if arg1 == arg2 {
+                vm.push(1f64);
+            } else {
+                vm.push(0f64);
+            }
+        }
+        Operator::Cgt => {
+            let (arg1, arg2) = vm.double_pop()?;
+            if arg1 > arg2 {
+                vm.push(1f64);
+            } else {
+                vm.push(0f64);
+            }
+        }
+        Operator::Clt => {
+            let (arg1, arg2) = vm.double_pop()?;
+            if arg1 < arg2 {
+                vm.push(1f64);
+            } else {
+                vm.push(0f64);
+            }
+        }
+
+        Operator::Stv(idx) => vm.store(idx)?,
+        Operator::Ldv(idx) => vm.load(idx)?,
     };
 
     Ok(())
 }
 
-fn parse_cmd(cmd: &str) -> Operator {
+fn parse_cmd(cmd: &str) -> Result<Operator, StackalcError> {
     match cmd {
         n if n.contains("ldc") => {
             let (_, arg) = n.split_once(":").unwrap();
-            Operator::Load(arg.parse::<f64>().unwrap())
+            Ok(Operator::Load(arg.parse::<f64>().unwrap()))
         }
-        "neg" => Operator::Neg,
-        "add" => Operator::Add,
-        "sub" => Operator::Sub,
-        "mul" => Operator::Mul,
-        "div" => Operator::Div,
-        _ => panic!(),
+
+        "neg" => Ok(Operator::Neg),
+        "add" => Ok(Operator::Add),
+        "sub" => Ok(Operator::Sub),
+        "mul" => Ok(Operator::Mul),
+        "div" => Ok(Operator::Div),
+        "dup" => Ok(Operator::Dup),
+        "pop" => Ok(Operator::Pop),
+
+        "ceq" => Ok(Operator::Ceq),
+        "cgt" => Ok(Operator::Cgt),
+        "clt" => Ok(Operator::Clt),
+
+        n if n.contains("ldv") => {
+            let (_, arg) = n.split_once(":").unwrap();
+            Ok(Operator::Ldv(arg.parse::<usize>().unwrap()))
+        }
+
+        n if n.contains("stv") => {
+            let (_, arg) = n.split_once(":").unwrap();
+            Ok(Operator::Stv(arg.parse::<usize>().unwrap()))
+        }
+
+        _ => Err(StackalcError::BadOperator),
     }
 }
 
 fn main() {
-    let mut stack = Vec::<f64>::new();
-
+    let mut vm = Interpreter::new();
     let mut buf = String::new();
 
     loop {
         io::stdin().read_line(&mut buf).unwrap();
         let line = buf.split_whitespace().map(|cmd| parse_cmd(cmd));
+        // .map(|op| {op.unwrap()});
 
         for op in line {
-            match execute_cmd(op, &mut stack) {
+            match op {
                 Err(err) => {
                     println!("[error] {:?}", err);
-                    break;
                 }
-                Ok(_) => {}
+                Ok(op) => match execute_cmd(op, &mut vm) {
+                    Err(err) => {
+                        println!("[error] {:?}", err);
+                        break;
+                    }
+                    Ok(_) => {}
+                },
             }
         }
 
-        println!("{:#?}", stack);
+        println!("{:#?}", vm.stack);
+        println!("{:#?}", vm.vars);
 
         buf.clear();
     }
